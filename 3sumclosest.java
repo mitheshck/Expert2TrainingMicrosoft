7005e4b422da6af8445f9ce1b8d2a0cef44f4e82
mitheshck <112608664+mitheshck@users.noreply.github.com>
2022-10-26 11:52:08 +0530

    Create 3sumclosest.java

---
+++ 3sumclosest.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans=nums[0]+nums[1]+nums[2];
+        
+        
+        for(int i=0;i<nums.length-2 ;i++){
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+               int sum=nums[i]+nums[j]+nums[k];
+            
+            
+                
+                if(Math.abs(sum-target)<=Math.abs(ans-target)){
+                    ans=sum;
+                }
+                if(sum==target){
+                    return sum; 
+                }
+                if(sum>target){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+            while((i<nums.length-3) && nums[i]==nums[i+1] ){
+                i++;
+            }
+        }
+        return ans;
+        
+    }
+}
